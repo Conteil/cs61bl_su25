ed008bb0216df5b7a46bed9af17d6bd473fe90f7
Conteil <Conteil@outlook.com>
2025-07-30 15:43:05 +0800

    Lab 04: Completed

--- lab04/tests/TriangleTest.java
+++ lab04/tests/TriangleTest.java
@@ -8,17 +8,35 @@ public abstract class TriangleTest {
 
     /* ***** TESTS ***** */
 
-    // FIXME: Add additional tests for Triangle.java here that pass on a
-    //  correct Triangle implementation and fail on buggy Triangle implementations.
-
     @Test
-    public void test1() {
-        // TODO: stub for first test
+    public void sidesFormTriangle() {
         Triangle t = getNewTriangle();
         // remember that you'll have to call on Triangle methods like
         // t.functionName(arguments), where t is a Triangle object
-
+        assertWithMessage("Any side must be positive.").that(t.sidesFormTriangle(0, 0, 0)).isEqualTo(false);
+        assertWithMessage("Any the sum of any two sides must be > the third side.").that(t.sidesFormTriangle(2, 2, 1)).isEqualTo(true);
+        assertWithMessage("Any the sum of any two sides must be > the third side.").that(t.sidesFormTriangle(3, 2, 1)).isEqualTo(false);
     }
 
+    @Test
+    public void pointsFormTriangle() {
+        Triangle t = getNewTriangle();
+        assertWithMessage("").that(t.pointsFormTriangle(1, 1, 2, 2, 3, 3)).isEqualTo(false);
+        assertWithMessage("").that(t.pointsFormTriangle(1, 1, 2, 2, 5, 4)).isEqualTo(true);
+    }
 
+    @Test
+    public void triangleType() {
+        Triangle t = getNewTriangle();
+        assertWithMessage("").that(t.triangleType(1, 2, 3)).isEqualTo("Scalene");
+        assertWithMessage("").that(t.triangleType(1, 1, 1)).isEqualTo("Equilateral");
+        assertWithMessage("").that(t.triangleType(1, 1, 2)).isEqualTo("Isosceles");
+    }
+
+    @Test
+    public void squaredHypotenuse() {
+        Triangle t = getNewTriangle();
+        assertWithMessage("").that(t.squaredHypotenuse(1, 2)).isEqualTo(5);
+        assertWithMessage("").that(t.squaredHypotenuse(3, 4)).isEqualTo(25);
+    }
 }
